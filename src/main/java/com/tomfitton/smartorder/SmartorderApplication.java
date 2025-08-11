4b385d4e81cab4db6c95d3543064d10c60dd5df6
Tom Fitton <thomas.fitton.17@gmail.com>
2025-08-11 10:44:53 +0100

    Initial commit

---
+++ src/main/java/com/tomfitton/smartorder/SmartorderApplication.java
@@ -0,0 +1,13 @@
+package com.tomfitton.smartorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartorderApplication.class, args);
+	}
+
+}
